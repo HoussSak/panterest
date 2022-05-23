97c5208e4a35a35962647819f9a3efbe86a1e3ee
HoussSak <99645587+HoussSak@users.noreply.github.com>
2022-05-24 01:52:30 +0200

    Add relation between pins and users

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -55,6 +55,11 @@ class Pin
      */
     private $imageName;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="pins")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
 
 
     public function getId(): ?int
@@ -116,5 +121,18 @@ class Pin
 
         return $this;
     }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
 
 }
